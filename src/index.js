5649a00fcdba80a35caeaee8f32f9262dfc55cbf
mywebchat <1009576562@qq.com>
2018-08-15 22:50:02 +0800

    0815

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import printMe from './print';
+// import _ from 'lodash';
+// import './style.css';
+// import Icon from './icon.png';
+// import Data from './data.xml';
+// import printMe from './print';
+import { cube } from './math.js';
+
 
 function component() {
-  var element = document.createElement('div');
-  let btn = document.createElement('button');
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  // var element = document.createElement('div');
+  // let btn = document.createElement('button');
+  // // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  // // Lodash, now imported by this script
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.classList.add('hello');
 
-  // let myIcon=new Image();
-  // myIcon.src=Icon;
-  // element.appendChild(myIcon);
+  // // let myIcon=new Image();
+  // // myIcon.src=Icon;
+  // // element.appendChild(myIcon);
 
-  console.log(Data);
+  // console.log(Data);
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick=printMe;
-  element.appendChild(btn);
+  // btn.innerHTML = 'Click me and check the console!';
+  // btn.onclick=printMe;
+  // element.appendChild(btn);
+
+
+  var element = document.createElement('pre');
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
   return element;
 }
 
